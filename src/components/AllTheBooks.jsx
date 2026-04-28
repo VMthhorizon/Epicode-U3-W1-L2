525b84569d8cb61f8adceace97e24431e4abf925
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-04-28 18:38:28 +0200

    ok

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -6,8 +6,8 @@ const AllTheBooks = function (props) {
       <Row className="d-flex g-4">
         {props.libri.map((libro, i) => {
           return (
-            <Col xs={12} md={4}>
-              <Card className="h-100" key={libro.asin + i}>
+            <Col xs={12} md={4} key={libro.asin + i}>
+              <Card className="h-100">
                 <Card.Img className="h-75" variant="top" src={libro.img} />
                 <Card.Body className="d-flex flex-column">
                   <Card.Title className="flex-grow-1">{libro.title}</Card.Title>
